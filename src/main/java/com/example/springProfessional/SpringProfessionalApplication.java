25282a2f010c97574475e9f436c510fe9dee662f
maduc <matheusduartec96@gmail.com>
2026-01-05 23:29:41 -0300

    Initial commit

---
+++ src/main/java/com/example/springProfessional/SpringProfessionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.springProfessional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProfessionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProfessionalApplication.class, args);
+	}
+
+}

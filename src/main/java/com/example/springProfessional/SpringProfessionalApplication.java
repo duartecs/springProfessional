f8f1cc4ea16940e93c919a8591f9cf790eb056ef
maduc <matheusduartec96@gmail.com>
2026-01-10 20:34:02 -0300

    DESAFIO: Modelo de dominio e ORM

--- src/main/java/com/example/springProfessional/SpringProfessionalApplication.java
+++ src/main/java/com/example/springProfessional/SpringProfessionalApplication.java
@@ -1,27 +1,13 @@
 package com.example.springProfessional;
 
-import com.example.springProfessional.entities.Order;
-import com.example.springProfessional.services.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringProfessionalApplication implements CommandLineRunner {
+public class SpringProfessionalApplication {
 
-    @Autowired
-    private OrderService orderService;
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringProfessionalApplication.class, args);
-	}
-
-    @Override
-    public void run(String... args) throws Exception {
-        Order order = new Order(1309, 95.90, 00.00);
-
-        System.out.println("Pedido codigo: " + order.getCode());
-        System.out.println("Valor total: R$ " + orderService.total(order));
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProfessionalApplication.class, args);
     }
+
 }
